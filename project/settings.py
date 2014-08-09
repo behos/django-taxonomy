98345ba115ef7049290a4d42cfd8e9c2c862cf40
Beherit <4.blood@gmail.com>
2014-08-09 13:06:30 +0100

    Commit project template

---
+++ project/settings.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+ADMINS = (
+    ('George', '4.blood@gmail.com'),
+)
+
+MANAGERS = ADMINS + (
+    ('Yiannis', 'disharmonic_dreams@hotmail.com'),
+)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '+q+pp6s0c!hj=&2tf^11r235rw1#jcb4kx)y58coi=px8nd=r4'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+TEMPLATE_DEBUG = False
+CAPTCHA_TEST_MODE = False
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+
+ALLOWED_HOSTS = ['moritsisremovals.ohmypixelhosting.com', '*.moritsisremovals.com']
+
+# Application definition
+
+INSTALLED_APPS = (
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'south',
+    'sorl.thumbnail',
+    'removal_manager',
+    'compressor',
+    'modeltranslation',
+    'tinymce',
+    'form_utils',
+    'jquery',
+    'bootstrap3_datetime',
+    'captcha',
+    'jquery_lightbox',
+    'seo',
+    'removals_seo',
+    'flatpages_i18n_tinymce',
+    'filebrowser',
+
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+
+ROOT_URLCONF = 'removals.urls'
+
+WSGI_APPLICATION = 'removals.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+try:
+    from local_db import DB_NAME, DB_USER, DB_PASS
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': DB_NAME,
+            'USER': DB_USER,
+            'PASSWORD': DB_PASS,
+        }
+    }
+
+except ImportError:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-gb'
+LANGUAGES = (
+    ('en', 'English'),
+    ('el', 'Greek'),
+)
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "removals/locale"),
+)
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "removals/static"),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # other finders..
+    'compressor.finders.CompressorFinder',
+)
+
+SITE_ID = 1
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "removals/templates"),
+)
+
+EMAIL_SUBJECT_PREFIX = ""
+SERVER_EMAIL = "noreply@moritsisremovals.com"
+DEFAULT_FROM_EMAIL = SERVER_EMAIL
+
+SEO_FOR_MODELS = [
+    'django.contrib.flatpages.models.FlatPage',
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+            'include_html': True,
+        },
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': BASE_DIR + '/log/error.log'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'removals': {
+            'handlers': ['logfile'],
+            'level': 'WARNING',
+            'propagate': False
+        },
+    },
+}
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': "advanced",
+    'relative_urls': False,
+    'plugins': "media",
+    'theme_advanced_buttons1_add': "media",
+    'theme_advanced_resizing': True,
+    'theme_advanced_statusbar_location': 'top'
+}
+
+try:
+    from local_settings import *  # NOQA
+except ImportError:
+    pass
+
+if 'test' in sys.argv or 'runserver' in sys.argv:
+    try:
+        from testing_settings import *  # NOQA
+    except ImportError:
+        pass
