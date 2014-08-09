c47977aa6811f3c6382c938e2bf5e2f438b5aa1d
Beherit <4.blood@gmail.com>
2014-08-09 23:16:26 +0100

    Add urls.py

---
+++ project/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+
+from django.contrib import admin
+from filebrowser.sites import site
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if 'tinymce' in settings.INSTALLED_APPS:
+    urlpatterns += patterns(
+        '',
+        url(r'^tinymce/', include('tinymce.urls')),
+        url(r'^admin/filebrowser/', include(site.urls)),
+    )
